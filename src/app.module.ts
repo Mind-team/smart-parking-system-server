b2658f50a85f889e3131075a07da5827fa686a2a
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2021-03-28 23:21:34 +0500

    Connect app with mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(new ConfigService().get('MONGODB_LINK')),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
