15cadbc28131056fbc127062e0c06ec915236168
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2021-03-30 01:38:11 +0500

    Create UserService and register method

---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { UserDocument } from '../../schemas/user.schema';
+import { Model } from 'mongoose';
+import { User } from '../../interfaces/user.interface';
+import { ServerResponse } from '../../models/server-request.model';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectModel('User')
+    private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  async register(userData: User) {
+    try {
+      await new this.userModel({ ...userData }).save();
+      return new ServerResponse('Success');
+    } catch (e) {
+      return new ServerResponse('Failed', e.message);
+    }
+  }
+}
