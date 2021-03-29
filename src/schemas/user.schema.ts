1acb10a860df3ebc26e1330fc825dbbb4c9e54ac
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2021-03-30 00:33:02 +0500

    Create user.schema.ts

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,13 @@
+import * as mongoose from 'mongoose';
+import { User } from '../interfaces/user.interface';
+
+export const UserSchema: mongoose.Schema<
+  mongoose.Document<User>
+> = new mongoose.Schema<mongoose.Document<User>>({
+  phoneNumber: { type: String, require: true, unique: true },
+  password: { type: String, require: true },
+  email: { type: String, unique: false, sparse: true },
+  plates: { type: Array, unique: true, sparse: true },
+});
+
+export type UserDocument = User & mongoose.Document;
