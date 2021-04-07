ec3195370835933fec92acd7fc65131c0901d5ad
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2021-04-07 19:39:47 +0500

    Start use ParkingRecord in User interface

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,6 +1,9 @@
+import { ParkingRecord } from './parking-record.interface';
+
 export interface User {
   phoneNumber: string;
   password: string;
   email?: string;
   plates: string[]; // TODO: When create plate model, change string[] on plates[]
+  parkingHistory: ParkingRecord[];
 }
