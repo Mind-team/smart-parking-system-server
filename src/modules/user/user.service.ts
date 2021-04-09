bda7c712d44b57f80385387a43bf428ee67d7804
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2021-04-09 08:44:32 +0500

    Start use ServerResponses in user service

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { UserDocument } from '../../schemas/user.schema';
 import { Model } from 'mongoose';
@@ -6,6 +6,9 @@ import { UserRecord } from '../../interfaces/user-record.interface';
 import * as bcrypt from 'bcrypt';
 import { SignInData } from '../../types/sign-in-data.type';
 import { User } from '../../models/user.model';
+import { FailedResponse } from '../../models/failed-response.model';
+import { SuccessfulResponse } from '../../models/successful-response.model';
+import { FilledSuccessfulResponse } from '../../models/filled-successful-response.model';
 
 @Injectable()
 export class UserService {
@@ -19,16 +22,20 @@ export class UserService {
       const [phoneNumber, password] = [userData.phoneNumber, userData.password];
       const candidate = await this.userModel.findOne({ phoneNumber });
       if (!candidate) {
-        return new HttpException(
-          `User with ${phoneNumber} phone number does not exist`,
+        return new FailedResponse(
           HttpStatus.BAD_REQUEST,
+          `User with ${phoneNumber} phone number does not exist`,
         );
       }
       return (await bcrypt.compare(password, candidate.password))
-        ? candidate
-        : new HttpException('Passwords do not match', HttpStatus.UNAUTHORIZED);
+        ? new FilledSuccessfulResponse<UserRecord>(
+            HttpStatus.OK,
+            'Successful login',
+            candidate,
+          )
+        : new FailedResponse(HttpStatus.UNAUTHORIZED, 'Password do not match');
     } catch (e) {
-      throw new HttpException('Something is wrong', HttpStatus.BAD_REQUEST);
+      throw new FailedResponse(HttpStatus.BAD_REQUEST, e.message);
     }
   }
 
@@ -37,9 +44,12 @@ export class UserService {
       // TODO: Нужно проверять, что все поля заполнены, кроме опциональных
       const newUser = await new User(userData).formatForDB();
       await new this.userModel({ ...newUser }).save();
-      return HttpStatus.CREATED;
+      return new SuccessfulResponse(
+        HttpStatus.CREATED,
+        'Successful registration',
+      );
     } catch (e) {
-      throw new HttpException(e.message, HttpStatus.BAD_REQUEST);
+      throw new FailedResponse(HttpStatus.BAD_REQUEST, e.message);
     }
   }
 
@@ -48,6 +58,7 @@ export class UserService {
   ) {
     // TODO: Валидация пользователя
     // TODO: Валидация номера введенного
+    // TODO: Проверка, что номер записался в пользователя
     try {
       const { phoneNumber, plate } = data;
       const document = await this.userModel.updateOne(
@@ -55,13 +66,16 @@ export class UserService {
         { $push: { plates: plate } },
       );
       return document.nModified === 0
-        ? new HttpException(
-            `User with ${phoneNumber} phone number does not exist`,
+        ? new FailedResponse(
             HttpStatus.BAD_REQUEST,
+            `User with ${phoneNumber} phone number does not exist`,
           )
-        : HttpStatus.OK;
+        : new SuccessfulResponse(
+            HttpStatus.OK,
+            'Plate number added successfully',
+          );
     } catch (e) {
-      throw new HttpException(e.message, HttpStatus.BAD_REQUEST);
+      throw new FailedResponse(HttpStatus.BAD_REQUEST, e.message);
     }
   }
 }
