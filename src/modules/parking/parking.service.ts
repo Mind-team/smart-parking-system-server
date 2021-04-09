c0ff3795c9f8453665dfd5892ac5627f9d41ed93
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2021-04-09 20:22:36 +0500

    Rename file: plain-parking-record -> parking-recorder

--- src/modules/parking/parking.service.ts
+++ src/modules/parking/parking.service.ts
@@ -2,7 +2,7 @@ import { HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { UserDocument } from '../../schemas/user.schema';
-import { ParkingRecorder } from '../../models/plain-parking-record.model';
+import { ParkingRecorder } from '../../models/parking-recorder.model';
 import { EntryCarParkingRecord } from '../../types/entry-car-parking-record.type';
 import { SuccessfulResponse } from '../../models/server-responses/successful-response.model';
 import { FailedResponse } from '../../models/server-responses/failed-response.model';
