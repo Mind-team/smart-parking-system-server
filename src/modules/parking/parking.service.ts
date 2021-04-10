5564ae459f1d8189f59156846ace2013db3eaef7
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2021-04-10 12:19:21 +0500

    Delete implementation registerCarEntry method

--- src/modules/parking/parking.service.ts
+++ src/modules/parking/parking.service.ts
@@ -2,7 +2,6 @@ import { HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { UserDocument } from '../../schemas/user.schema';
-import { ParkingRecorder } from '../../models/parking-recorder.model';
 import { EntryCarParkingRecord } from '../../types/entry-car-parking-record.type';
 import { SuccessfulResponse } from '../../models/server-responses/successful-response.model';
 import { FailedResponse } from '../../models/server-responses/failed-response.model';
@@ -15,26 +14,28 @@ export class ParkingService {
   ) {}
 
   async registerCarEntry(data: EntryCarParkingRecord) {
-    try {
-      const user = await this.userModel.findOne({ plates: data.carPlate });
-      if (!user) {
-        // Обработка чела, которого нет в бд
-        return;
-      }
-      user.parkingHistory.push(
-        await new ParkingRecorder(
-          data.parkingTitle,
-          data.carPlate,
-          data.entryCarTime,
-        ).formatForDB(),
-      );
-      user.save();
-      return new SuccessfulResponse(
-        HttpStatus.CREATED,
-        'Successfully registered the entry of the car',
-      );
-    } catch (e) {
-      throw new FailedResponse(HttpStatus.BAD_REQUEST, e.message);
-    }
+    // TODO: Refactoring
+    // try {
+    //   const user = await this.userModel.findOne({ plates: data.carPlate });
+    //   if (!user) {
+    //     // Обработка чела, которого нет в бд
+    //     return;
+    //   }
+    //   user.parkingHistory.push(
+    //     await new ParkingRecorder(
+    //       data.parkingTitle,
+    //       data.carPlate,
+    //       data.entryCarTime,
+    //     ).formatForDB(),
+    //   );
+    //   user.save();
+    //   return new SuccessfulResponse(
+    //     HttpStatus.CREATED,
+    //     'Successfully registered the entry of the car',
+    //   );
+    // } catch (e) {
+    //   throw new FailedResponse(HttpStatus.BAD_REQUEST, e.message);
+    // }
+    throw new FailedResponse(HttpStatus.BAD_REQUEST, 'Fail');
   }
 }
