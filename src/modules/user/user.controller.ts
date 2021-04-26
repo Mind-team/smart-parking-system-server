2f8a7e7e6069bedb6c508b5ee86aafda57cf1c0d
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2021-04-27 02:28:36 +0500

    Start use dto in signIn

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,16 +1,19 @@
-import { Controller, Post, Body, Get, Query } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { UserService } from './user.service';
-import { UserRecord } from '../../interfaces/records/user-record.interface';
 import { SignInData } from '../../types/sign-in-data.type';
 import { SignUpData } from '../../types/sign-up-data.type';
+import { SignInDto } from '../../dtos/sign-in.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('signIn')
-  async signInUser(@Body() data: SignInData) {
-    return await this.userService.signIn(data);
+  async signInUser(@Body() data: SignInDto) {
+    return await this.userService.signIn({
+      phoneNumber: { value: data.phoneNumber },
+      password: data.password,
+    });
   }
 
   @Post('signUp')
