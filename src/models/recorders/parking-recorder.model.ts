62045dba4631dddaa55d57427bd2689262e8df28
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2021-05-07 13:21:25 +0500

    Small refactoring in parking-recorder

--- src/models/recorders/parking-recorder.model.ts
+++ src/models/recorders/parking-recorder.model.ts
@@ -2,14 +2,21 @@ import { ParkingRecord } from '../../interfaces/records/parking-record.interface
 import { Recorder } from '../../interfaces/recorder.interface';
 
 export class ParkingRecorder implements Recorder<ParkingRecord> {
-  public formatForDB(model: ParkingRecord): ParkingRecord {
+  public formatForDB({
+    carPlate,
+    departureCarTime,
+    entryCarTime,
+    parkingTitle,
+    priceRub,
+    parkingTimeMin,
+  }: ParkingRecord): ParkingRecord {
     return {
-      carPlate: model.carPlate,
-      departureCarTime: model.departureCarTime,
-      entryCarTime: model.entryCarTime,
-      parkingTitle: model.parkingTitle,
-      priceRub: model.priceRub,
-      parkingTimeMin: model.parkingTimeMin,
+      carPlate,
+      departureCarTime,
+      entryCarTime,
+      parkingTitle,
+      priceRub,
+      parkingTimeMin,
     };
   }
 }
