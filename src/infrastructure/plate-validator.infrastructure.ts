2708565d625ee1ae07cedb43826a23a47b91846c
Ilya <Ermolaev.ID@yandex.ru>
2021-07-09 18:14:51 +0500

    Start use regex in plate validator

--- src/infrastructure/plate-validator.infrastructure.ts
+++ src/infrastructure/plate-validator.infrastructure.ts
@@ -2,16 +2,8 @@ import { Validator } from '../models/validator.interface';
 
 export class PlateValidator implements Validator<string> {
   isValid(plate: string): boolean {
-    if (
-      plate.length === 6 &&
-      !isNaN(Number(plate.substring(1, 3))) &&
-      isNaN(Number(plate[0])) &&
-      isNaN(Number(plate[4])) &&
-      isNaN(Number(plate[5]))
-    ) {
-      return true;
-    }
-    return false;
+    const regex = /[а-я]\d\d\d[а-я][а-я]/;
+    return !(plate.length !== 6 || !regex.test(plate));
   }
 
   tryFormat(plate: string): string {
