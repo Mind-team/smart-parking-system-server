1f0a26b0237ed0a85c969bfc7914f09d9f16c1bd
Ilya <Ermolaev.ID@yandex.ru>
2021-07-09 13:47:38 +0500

    Update standard-price-calculator.infrastructure.ts

--- src/infrastructure/standard-price-calculator.infrastructure.ts
+++ src/infrastructure/standard-price-calculator.infrastructure.ts
@@ -2,6 +2,6 @@ import { PriceCalculator } from '../models/price-calculator.interface';
 
 export class StandardPriceCalculator implements PriceCalculator {
   calculate(parkingTitle: string, parkingTimeMin: number): number {
-    return 100 * parkingTimeMin;
+    return 100 * Math.round(parkingTimeMin);
   }
 }
