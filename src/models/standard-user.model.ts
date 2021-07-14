375379cee2e58f81d98d61416ec16651353a7192
Ilya <Ermolaev.ID@yandex.ru>
2021-07-15 01:01:42 +0500

    Connect UnregisteredStandardUser with StandardUser

--- src/models/standard-user.model.ts
+++ src/models/standard-user.model.ts
@@ -4,13 +4,15 @@ import { PhoneNumber } from './interfaces/phone-number.interface';
 import { Plate } from './interfaces/plate.interface';
 import { Parking } from './interfaces/parking.interface';
 import { RegisteredUserContent } from './interfaces/registered-user-content.interface';
+import { UnregisteredStandardUser } from './unregistered-standard-user.model';
 
-export class StandardUser implements User<'Registered'> {
+export class StandardUser
+  extends UnregisteredStandardUser
+  implements User<'Registered'>
+{
   readonly #phoneNumber: PhoneNumber;
   readonly #email?: string;
   readonly #password: string;
-  readonly #plates: UniqueArray<Plate>;
-  readonly #parkings: Parking[];
 
   constructor(
     phoneNumber: PhoneNumber,
@@ -19,36 +21,18 @@ export class StandardUser implements User<'Registered'> {
     parkings: Parking[],
     email?: string,
   ) {
+    super(plates, parkings);
     this.#phoneNumber = phoneNumber;
     this.#password = password;
-    this.#plates = plates;
-    this.#parkings = parkings;
     this.#email = email;
   }
 
-  addPlate(plate: Plate) {
-    this.#plates.push(plate);
-  }
-
-  registerParking(parking: Parking) {
-    this.#parkings.push(parking);
-  }
-
-  lastParking(popOrPeek: 'pop' | 'peek') {
-    if (popOrPeek === 'pop') {
-      return this.#parkings.pop();
-    }
-    const last = this.#parkings.pop();
-    this.#parkings.push(last);
-    return last;
-  }
-
-  content(): RegisteredUserContent {
+  override content(): RegisteredUserContent {
     return {
       phoneNumber: this.#phoneNumber.value,
       password: this.#password,
-      plates: this.#plates.map((plate) => plate.value),
-      parkings: this.#parkings.map((parking) => parking.content()),
+      plates: super.content().plates,
+      parkings: super.content().parkings,
       email: this.#email,
     };
   }
