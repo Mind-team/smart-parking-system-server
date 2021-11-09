288119c6581941c3157925ac712086d966a70704
Ilya <Ermolaev.ID@yandex.ru>
2021-11-10 00:29:59 +0500

    feat(parkingModule): add auto api

--- src/modules/parking/dto/register-car-entry.dto.ts
+++ src/modules/parking/dto/register-car-entry.dto.ts
@@ -1,5 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class RegisterCarEntryDto {
+  @ApiProperty({
+    type: String,
+    description: 'Parking owner id',
+    required: true,
+  })
   parkingOwnerId: string;
+
+  @ApiProperty({
+    type: String,
+    description: 'Car plate',
+    required: true,
+  })
   carPlate: string;
+
+  @ApiProperty({
+    type: String,
+    description: 'Entry car time',
+    required: true,
+  })
   entryCarTime: string;
 }
