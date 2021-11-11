88c6934d6258cce53fb9cdb4a35b6debf7a8126d
Ilya <Ermolaev.ID@yandex.ru>
2021-11-11 18:08:28 +0500

    ref(apis): delete

--- apis/dto/parking.dto.ts
+++
@@ -1,9 +0,0 @@
-interface ParkingDto {
-  parkingOwnerId: string;
-  parkingTitle: string;
-  carPlate: string;
-  entryCarTime: string;
-  departureCarTime: string | null;
-  priceRub: number | null;
-  isCompleted: boolean;
-}
--- apis/dto/user.dto.ts
+++
@@ -1,7 +0,0 @@
-interface UserDto {
-  phoneNumber: string;
-  password: string;
-  email?: string;
-  plates: string[];
-  parkings: ParkingDto[];
-}
